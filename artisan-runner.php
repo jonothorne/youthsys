ff99993dbcb9ddbca65afa98e640f009828a7997
Jono Thorne <jonothorne@Jonos-MacBook-Air.local>
2026-02-16 17:06:15 +0000

    Update artisan runner to include key/migrate/seed

--- artisan-runner.php
+++ artisan-runner.php
@@ -1,13 +1,19 @@
 <?php
 use Illuminate\Contracts\Console\Kernel;
 
+if (php_sapi_name() !== 'cli') {
+    header('Content-Type: text/plain');
+}
+
 require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 try {
+    $kernel->call('key:generate', ['--force' => true]);
     $kernel->call('migrate', ['--force' => true]);
-    echo "Migrations complete\n";
+    $kernel->call('db:seed', ['--force' => true]);
+    echo "Application key set, migrations and seeds complete.";
 } catch (Exception $e) {
     http_response_code(500);
     echo 'Error: '.$e->getMessage();
